5bed5f20c1447a752ea960e2b4f7610bad7ad601
haosappro <haop7078@gmail.com>
2022-04-14 17:02:41 +0700

    first commit

---
+++ PhanTuanHao_WebBanHang/WebBanHang/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Context;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        WebsiteBanHangEntities objwebsiteBanHangEntities = new WebsiteBanHangEntities();
+        public ActionResult Index()
+        {
+            HomeModel objHomeModel = new HomeModel();
+            objHomeModel.ListCategory = objwebsiteBanHangEntities.Categories.ToList();
+
+            objHomeModel.ListProduct = objwebsiteBanHangEntities.Products.ToList();
+            return View(objHomeModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
